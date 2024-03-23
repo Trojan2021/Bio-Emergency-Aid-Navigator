bd807a03e30cdae46b8491599da1d634af8a7e00
Gnerone <99757819+Gnerone@users.noreply.github.com>
2024-03-23 14:21:58 -0400

    Add files via upload

---
+++ PublishIO.py
@@ -0,0 +1,31 @@
+import time
+from Adafruit_IO import Client, Feed
+
+ADAFRUIT_IO_USERNAME = 'g_nerone'
+ADAFRUIT_IO_KEY = 'aio_bcim25nKA0sL4DpB82cbevOCaXGH'
+
+FEED_NAME = 'test'
+
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY) 
+
+def publish_to_feed(value):
+    aio.send_data(FEED_NAME, value)
+while True:
+
+    value = input("Enter 0 for close or 1 for open (q to quit): ")
+    
+    if value.lower() == 'q':
+        break
+
+    try:
+        value = int(value)
+    except ValueError:
+        print("Invalid input. Please enter 0, 1, or q to quit.")
+        continue
+    if value not in [0, 1]:
+        print("Invalid input. Please enter 0 or 1.")
+    else:
+
+        publish_to_feed(value)
+        print(f"Published value {value} to Adafruit IO feed")
+        
